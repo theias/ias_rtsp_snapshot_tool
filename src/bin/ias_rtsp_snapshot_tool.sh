5c4a5455bc7f1625b86b296317de4a963da280da
Martin VanWinkle <mvanwinkle@ias.edu>
2018-05-16 09:58:35 -0400

    committing

--- src/bin/ias_rtsp_snapshot_tool.sh
+++ src/bin/ias_rtsp_snapshot_tool.sh
@@ -7,6 +7,8 @@ function debug
 	>&2 echo "Username: " $username
 	>&2 echo "Hostname: " $hostname
 	>&2 echo "Output dir: " $output_dir
+	>&2 echo "scene_prefix: " $scene_prefix
+	>&2 echo "camera_config_file_without_extension:" $camera_config_file_without_extension
 }
 
 function usage
@@ -22,21 +24,24 @@ function usage
 	echo ""
 	echo "-- Config Examples --"
 	echo '    credentials:   {"username":"someuser","password":"somepass"}'
-	echo '    camera config: { "hostname":"some.hostname.example.com", "output_dir":"/var/somepath"} '
+	echo '    camera config: { "hostname":"some.hostname.example.com", "output_dir":"/var/somepath", "scene-prefix":"img"} '
 
 	echo ""
 	echo "-- Mandatory config parameters --"
+	echo "Credentials:"
 	echo "    username"
 	echo "    password"
+	echo "Camera config:"
 	echo "    hostname"
 
 	echo ""
 	echo "-- Optional config parameters --"
+	echo "Camera config:"
 	echo "    output_dir - defaults to /var/tmp/rtsp_snapshots"
-
+	echo "    scene-prefix - defaults to img"
 	echo ""
 	echo "-- Requires --"
-	echo "    cvlc"
+	echo "    cvlc (vlc-core on rpm systems(?))"
 	echo "    x264"
 	echo "    jq"
 }
@@ -63,7 +68,29 @@ jq=jq
 username=`$jq --raw-output '.username' "$json_credentials_file"`
 password=`$jq --raw-output '.password' "$json_credentials_file"`
 hostname=`$jq --raw-output '.hostname' "$camera_config_file"`
-output_dir=`$jq --raw-output '.output_dir' "$camera_config_file" 2>/dev/null`
+
+scene_prefix=`$jq --raw-output '.scene-prefix' "$camera_config_file" 2>/dev/null`
+
+camera_config_file_without_extension="${camera_config_file%.*}"
+camera_config_file_without_extension="${camera_config_file_without_extension##*/}"
+
+if [[ -z "$scene_prefix" ]]
+then
+	prefix_date=`date '+%Y-%m-%d-%H-%M-%S'`
+	scene_prefix="$camera_config_file_without_extension--$prefix_date--snapshot"
+fi
+
+output_dir="$rtsp_shapshot_output_dir"
+
+if [[ -z "$output_dir" ]]
+then
+	output_dir=`$jq --raw-output '.output_dir' "$camera_config_file" 2>/dev/null`
+
+	if [[ "$output_dir" == "null" ]]
+	then
+		unset output_dir
+	fi
+fi
 
 if [[ -z "$username" ]]
 then
@@ -86,13 +113,12 @@ then
 	exit 1
 fi
 
+
 if [[ -z "$output_dir" ]]
 then
 	output_dir=/var/tmp/rtsp_screenshots
 fi
 
-
-output_dir="$output_dir"/`date '+%Y-%m-%d-%H-%M-%S'`
 
 if [[ ! -z "$debug_camera_snapshot" ]]
 then
@@ -103,11 +129,11 @@ mkdir -p "$output_dir"
 
 cvlc rtsp://$username:$password@$hostname \
 --video-filter=scene \
---scene-prefix=img \
+--scene-prefix="$scene_prefix" \
 --scene-format=jpg \
 --scene-path="$output_dir" \
 --scene-replace \
 --scene-ratio 120 \
 --vout=dummy \
---run-time 5 \
+--run-time 10 \
 vlc://quit
